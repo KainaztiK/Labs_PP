212eb8a947b8474501df873cdbb365e0ebd991fa
KainaztiK <falileev.sergey2003@yandex.ru>
2022-10-05 10:39:07 +0300

    Lab2

---
+++ CompanyEmployess/CompanyEmployess/Migrations/20221005053227_InitialData1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompanyEmployess.Migrations
+{
+    public partial class InitialData1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CompanyEmployess/Entities/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    public class Client
+    {
+        [Column("ClientId")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Client ID is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Client FIO is 60 characters.")]
+        public string Client_FIO { get; set; }
+    }
+}
